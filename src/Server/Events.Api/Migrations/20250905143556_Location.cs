b6ff6314ff238a6574a61f20f97f906dfb0b1b9c
Marcus <marcus.johnsson@campusnykoping.se>
2025-09-05 16:36:27 +0200

    Location added to bank

---
+++ src/Server/Events.Api/Migrations/20250905143556_Location.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Events.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Location : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Events");
+        }
+    }
+}
