1593510f0e036570efe9650c536875ab29151352
Marcus Johnsson <marcus.johnsson@campusnykoping.se>
2025-09-11 01:55:53 +0200

    Updated grid

--- src/client/src/lib/components/getCategoryTable.svelte
+++ src/client/src/lib/components/getCategoryTable.svelte
@@ -2,39 +2,88 @@
     import ApiService from "$lib/services/apiService";
     import { CategoryPostService } from "$lib/services/category/GetCategory";
     import { onMount } from "svelte";
+    import { updateCategory } from "$lib/services/category/updateCategory";
+    import type { UpdateCategoriesData } from "$lib/services/category/PatchCategory";
+    import { text } from "@sveltejs/kit";
+    import {CategoryDeleteService} from "$lib/services/category/DeleteCategory";
 
+    let choosenCategory: number | null = null;
+    let title = "";
+    let deleteId : null | number = null;
+    let selectedCategory = "";
+   
     const apiService = new ApiService();
-    
     interface Category{
         title: string;
         id: number;
     }
     let categories: Category[] = [];
+    const categoryDeleteService = new CategoryDeleteService(apiService);
 
     onMount(async () => {
     categories = await new CategoryPostService(apiService).GetCategories();
     });
-
+    async function confirmOption() {
+    if (deleteId === null) {
+      const updatedCategory: UpdateCategoriesData = {
+        id: parseInt(selectedCategory),
+        title
+      };
+      updateCategory(updatedCategory);
+    } else {
+     await categoryDeleteService.deleteCategory({id: deleteId});
+    }
+    choosenCategory = null; // Reset after updating
+  }
 </script>
 
 <table>
-    <thead>
-        <tr>
-            <th>Categories</th>
-        </tr>
-    </thead>
-    <tbody>
-        {#each categories as category}
-            <tr style="background-color: {category.id % 2 === 0 ? 'rgb(95, 62, 62)' : 'rgb(124, 100, 56)'}">
-                <td>{category.id}. {category.title}</td>
-
-                <td>Ändra</td>
-                <td>Ta bort</td>
-            </tr>
-            
-        {/each}
-    </tbody>
+  <thead>
+    <tr>
+      <th>Categories</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each categories as category}
+      <tr style="background-color: {category.id % 2 === 0 ? 'rgb(95, 62, 62)' : 'rgb(124, 100, 56)'}">
+        {#if choosenCategory !== category.id && deleteId != category.id}
+          <td>{category.title}</td>
+          <td>
+            <button on:click={() => {
+              choosenCategory = category.id;
+              title = category.title; 
+            }}> Ändra</button>
+          </td>
+          <td>
+            <button on:click={() => {
+              choosenCategory = category.id;
+              deleteId = category.id;
+            }}>Remove</button>
+          </td>
+        {:else}
+          <td>
+            <input type="text" bind:value={title}/>
+          </td>
+          <td>
+            <button on:click={() => {
+              const updatedCategory: UpdateCategoriesData = {
+                id: category.id,
+                title
+              };
+              updateCategory(updatedCategory);
+              choosenCategory = null;
+            }}>  {deleteId !== null ? 'Delete' : 'Save'}
+            </button>
+          </td>
+          <td>
+            <button on:click={() => {choosenCategory = null, deleteId = null} }>Cancel</button>
+          </td>
+        {/if}
+      </tr>
+    {/each}
+  </tbody>
 </table>
+
 
 <style>
       th {
---
+++ src/client/src/lib/services/category/updateCategory.ts
@@ -0,0 +1,11 @@
+import ApiService from "$lib/services/apiService";
+import { EventPostService } from "$lib/services/category/PatchCategory";
+import type { UpdateCategoriesData } from "$lib/services/category/PatchCategory";
+
+const apiService = new ApiService();
+
+const eventPostService = new EventPostService(apiService);
+
+export async function updateCategory(data: UpdateCategoriesData) {
+    await eventPostService.patchEvent(data);
+}
